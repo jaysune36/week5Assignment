53e74536ef50b12f501affbaa91ccc63542a7f17
user <user@users-MacBook-Pro.local>
2023-06-27 15:20:51 -0700

    created 3 functions within Menu class(addShow, deleteShow, viewShow). All functions do seem to work properly. Will work to create 2 new functions Add Characters and view characters when a show is selected

--- main.js
+++ main.js
@@ -28,7 +28,8 @@ class CartoonChar {
 
 class Menu {
   constructor() {
-
+    this.shows = [];
+    this.cartoonSelected = null;
   }
 
   start() {
@@ -59,6 +60,57 @@ class Menu {
     3) View Show List
     4) Exit
     `);
+  }
+
+  addShow() {
+    let show = prompt(`
+      What is the name of Cartoon you would like to add?
+    `);
+    this.shows.push(new Cartoon(show));
+  }
+
+  deleteShow() {
+    let listStr = '';
+    if(this.shows.length != 0) {
+    for(let i=0; i<this.shows.length; i++) {
+      listStr += i + 1 + ') ' + this.shows[i].name + ' \n';
+    }
+    let select = prompt('Please select a Cartoon to delete \n-------------------------------- \n' + listStr );
+    alert(`
+    ${this.shows[select - 1].name} has successfully been deleted!
+    `)
+    this.shows.splice(select - 1, 1);
+    } else {
+      alert(`
+      There are no Cartoon shows to delete.
+      `)
+    }
+  }
+
+  viewShowList() {
+    let listStr = '';
+    if(this.shows.length != 0) {
+    for(let i=0; i<this.shows.length; i++) {
+      listStr += i + 1 + ') ' + this.shows[i].name + ' \n';
+    }
+    let select = prompt('1) Select Cartoon to Add Characters \n2) Select Cartoon to View Characters \n3) Delete Cartoon\n -------------------------------- \n' + listStr );
+    switch(select) {
+      case '1':
+        this.addCartoonChar();
+        break;
+      case '2':
+        this.viewCharList();
+        break;
+      case '3':
+        this.deleteShow();
+        break;
+      default:
+        this.showMainMenu();
+    }
+
+    } else {
+      alert(`Please add a cartoon before viewing your list`)
+    }
   }
 
 }
