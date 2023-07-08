776a51f5483802758a1f0ceb9a29c6d420166ed9
user <user@users-MacBook-Pro.local>
2023-07-07 18:55:24 -0700

    restructed class loops for making a list into a function outside of class list. Changed these instances under the class methods(deleteShowChar, viewShowList, viewCharList). Reran code and does seem to work, will continue to test incase of any bugs

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+function makeList(arr) {
+  let listStr = '';
+  for (let i = 0; i < arr.length; i++) {
+    listStr += i + 1 + ') ' + arr[i].name + ' \n';
+  }
+  return listStr;
+}
+
 class Cartoon {
   constructor(name) {
     this.name = name;
@@ -64,8 +72,8 @@ class Menu {
 
   addShow() {
     let show = prompt(`
-      What is the name of Cartoon you would like to add?
-    `);
+      What is the name of Cartoon you would like to add? 
+      `);
     this.shows.push(new Cartoon(show));
   }
 
@@ -88,13 +96,8 @@ class Menu {
   }
 
   deleteShowChar() {
-    // console.log(this.shows[this.cartoonSelected - 1].charName.length)
-    let listStr = '';
     if (this.shows[this.cartoonSelected - 1].charName.length != 0) {
-      for (let i = 0; i<this.shows[this.cartoonSelected - 1].charName.length; i++) {
-        listStr += i + 1 + ') ' + this.shows[this.cartoonSelected - 1].charName[i].name + ' \n';
-      }
-      let select = prompt('Please select a Cartoon to delete \n-------------------------------- \n' + listStr);
+      let select = prompt('Please select a Cartoon to delete \n-------------------------------- \n' + makeList(this.shows[this.cartoonSelected - 1].charName));
       alert(`
     ${this.shows[this.cartoonSelected - 1].charName[select - 1].name} has successfully been deleted!
     `)
@@ -107,61 +110,18 @@ class Menu {
   }
 
   viewShowList() {
-    let listStr = '';
     if (this.shows.length != 0) {
-      for (let i = 0; i < this.shows.length; i++) {
-        listStr += i + 1 + ') ' + this.shows[i].name + ' \n';
-      }
-      // let select = prompt('1) Select a Cartoon to view the Characters \n 2) Go Back \n -------------------------------- \n' + listStr );
-      // if(select -1 <= this.shows.length) {
-      // prompt('1) Add Character to ' + this.shows[select - 1].name + ' \n2) Select Cartoon to View Characters \n3) Delete Cartoon\n -------------------------------- \n' + listStr );
-      let select = prompt(' 1) Select a Cartoon to view the Characters \n 2) Go Back \n -------------------------------- \n' + listStr);
-      if(select - 1 <= this.shows.length) {
+      let select = prompt(' Select a Cartoon to view the Characters \n Or press "Enter" to return to the Main Menu\n -------------------------------- \n' + makeList(this.shows));
+      if(select - 1 <= this.shows.length && select !== '') {
         this.cartoonSelected = select;
-        return this.viewShow()
-      } else {
-        return this.showMainMenu();
+        return this.showDetails()
       }
-      // switch (select) {
-      //   case '1':
-      //     this.cartoonSelected = select;
-      //     this.viewShow();
-      //     break;
-      //   case '2':
-      //     this.cartoonSelected = select;
-      //     this.deleteShow();
-      //     break;
-      //   // case '3':
-      //   //   this.deleteShow();
-      //   //   break;
-      //   default:
-      //     this.showMainMenu();
-      // }
-      // } else {
-      //   return this.showMainMenu()
-      // }
-      // let select = prompt('1) Select Cartoon to Add Characters \n2) Select Cartoon to View Characters \n3) Delete Cartoon\n -------------------------------- \n' + listStr );
-      // switch(select) {
-      //   case '1':
-      //     this.cartoonSelected = select;
-      //     this.addCartoonChar();
-      //     break;
-      //   case '2':
-      //     this.cartoonSelected = select;
-      //     this.viewCharList();
-      //     break;
-      //   case '3':
-      //     this.deleteShow();
-      //     break;
-      //   default:
-      //     this.showMainMenu();
-      // }
     } else {
       alert(`Please add a cartoon before viewing your list`)
     }
   }
 
-viewShow() {  
+showDetails() {  
   let select = prompt(this.shows[this.cartoonSelected - 1].name + ' selected \n1) Add Characters \n2) View Characters \n3) Delete Characters\n -------------------------------- \n');
   switch (select) {
     case '1':
@@ -181,15 +141,12 @@ viewShow() {
 addCartoonChar() {
   let char = prompt(`What Character would you like to add to ${this.shows[this.cartoonSelected - 1].name}`)
   this.shows[this.cartoonSelected - 1].addChar(new CartoonChar(char));
-  alert(`${char} has successfully been added as a character to the ${this.shows[this.cartoonSelected - 1].name} list: ${this.shows[this.cartoonSelected - 1].charName.length}`);
+  alert(`${char} has successfully been added as a character to the ${this.shows[this.cartoonSelected - 1].name} list`);
+  return this.showDetails()
 }
 viewCharList() {
-  let listStr = '';
-  if (this.shows.length != 0) {
-    for (let i = 0; i < this.shows[this.cartoonSelected - 1].charName.length; i++) {
-      listStr += i + 1 + ') ' + this.shows[this.cartoonSelected - 1].charName[i].name + ' \n';
-    }
-    let select = prompt('1) Add another Character \n2) Delete Character\n -------------------------------- \n' + listStr);
+  if (this.shows[this.cartoonSelected - 1].charName.length != 0) {
+    let select = prompt('1) Add another Character \n2) Delete Character \n Or press "Enter" to return to the Main Menu\n -------------------------------- \n' + makeList(this.shows[this.cartoonSelected - 1].charName));
     switch (select) {
       case '1':
         this.addCartoonChar();
